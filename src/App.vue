4536e6a906961566cd9c9e7fe97535af4744fd84
iknowmagic <ilan@iknowmagic.net>
2020-08-16 10:39:02 -0700

    job listing progress

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,39 @@
         <div class="shape s2"></div>
       </div>
     </div>
+    <div class="main-body">
+      <template v-for="job in jobListings">
+        <job-listing :key="job.id" :job="job" />
+      </template>
+    </div>
     <div class="main-footer"></div>
   </div>
 </template>
 
 <script>
+// @flow
+
+import JobListing from '@/components/JobListing'
+import axios from 'axios'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    JobListing
+  },
+  data() {
+    return {
+      jobListings: undefined
+    }
+  },
+  mounted() {
+    this.getJobListings()
+  },
+  methods: {
+    async getJobListings() {
+      const { data } = await axios.get(`/data.json`)
+      this.jobListings = data
+    }
+  }
 }
 </script>
---
+++ src/components/JobListing.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="job-listing">
+    <div class="job-logo">
+      <img :src="job.logo" />
+    </div>
+    <div class="job-info">
+      <div class="job-info-company">
+        {{ job.company }}
+      </div>
+      <div class="job-info-pills">
+        <div v-if="job.new" class="job-info-pill info-new">
+          New!
+        </div>
+        <div v-if="job.featured" class="job-info-pill info-feat">
+          Featured
+        </div>
+      </div>
+    </div>
+    <div class="job-title">
+      {{ job.position }}
+    </div>
+    <div class="job-info2">
+      <div class="job-info2-item">
+        {{ job.postedAt }}
+      </div>
+      <div class="job-info2-item">
+        {{ job.contract }}
+      </div>
+      <div class="job-info2-item">
+        {{ job.location }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @flow
+
+export default {
+  name: 'JobListing',
+  props: {
+    job: {
+      type: Object,
+      default: undefined
+    }
+  }
+}
+</script>
