e91d1dce5d945a9524c1140a9bfde4e66baee76c
iknowmagic <ilan@iknowmagic.net>
2020-08-23 12:47:24 -0700

    init myteam project

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,20 @@
 <template>
   <div class="main-container">
     <div class="main-header">
-      <div class="header-shapes">
-        <div class="shapes-d">
-          <div class="shape d1"></div>
-          <div class="shape d2"></div>
-        </div>
-        <div class="shapes-s">
-          <div class="shape s1"></div>
-          <div class="shape s2"></div>
-        </div>
+      <div class="main-header-logo">
+        <img src="@/assets/images/logo.svg" alt="logo" />
+      </div>
+      <div class="main-header-menu-trigger">
+        <img src="@/assets/images/icon-hamburger.svg" alt="logo" />
       </div>
     </div>
-    <div class="main-body">
-      <div v-if="filterTags.length > 0" class="filters">
-        <div class="filter-tags">
-          <template v-for="filterTag in filterTags">
-            <div :key="filterTag" class="filter-tag">
-              <div class="filter-tag-text">
-                {{ filterTag }}
-              </div>
-              <div class="filter-tag-close" @click="deleteFilterTag(filterTag)">
-                <div class="icon-cancel"></div>
-              </div>
-            </div>
-          </template>
-        </div>
-        <div class="filter-clear" @click="filterTags = []">
-          <div class="btn-clear">
-            Clear
-          </div>
-        </div>
-      </div>
-      <template v-for="job in jobListings">
-        <job-listing v-if="isListingIncluded(job)" :key="job.id" :job="job" />
-      </template>
-    </div>
+    <div class="main-body"></div>
   </div>
 </template>
 
 <script>
 // @flow
 
-import JobListing from '@/components/JobListing'
 import { call, get, sync } from 'vuex-pathify'
 
 import _cloneDeep from 'lodash/cloneDeep'
@@ -51,37 +23,9 @@ import _intersection from 'lodash/intersection'
 
 export default {
   name: 'App',
-  components: {
-    JobListing
-  },
-  computed: {
-    jobListings: get('jobs/jobListings'),
-    filterTags: sync('jobs/filterTags')
-  },
-  mounted() {
-    this.getJobListings()
-  },
-  methods: {
-    getJobListings: call('jobs/getJobListings'),
-    isListingIncluded(job) {
-      if (this.filterTags.length === 0) return true
-      const common = _intersection(this.filterTags, this.getJobFeatures(job))
-      return common.length === this.filterTags.length
-    },
-    getJobFeatures(job) {
-      const result = []
-      result.push(job.role)
-      result.push(job.level)
-      result.push(...job.languages)
-      result.push(...job.tools)
-      return result
-    },
-    deleteFilterTag(filterTag: string) {
-      const filterTags = _cloneDeep(this.filterTags)
-      this.filterTags = filterTags.filter(item => {
-        return item !== filterTag
-      })
-    }
-  }
+  components: {},
+  computed: {},
+  mounted() {},
+  methods: {}
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,38 @@
 module.exports = {
   configureWebpack: {
-    name: 'myteam'
+    name: 'myteam',
     /**
      * https://webpack.js.org/configuration/devtool/#devtool
      * https://blog.scottlogic.com/2017/11/01/webpack-source-map-options-quick-guide.html
      * https://cli.vuejs.org/guide/mode-and-env.html#modes
      */
     // devtool: process.env.NODE_ENV === 'production' ? 'none' : 'eval-source-map'
+
+    output:
+      process.env.NODE_ENV === 'production'
+        ? {
+            filename: 'js/[name].[contenthash:8].js',
+            chunkFilename: 'js/chunk-[contenthash:8].js'
+          }
+        : {}
+  },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+
+    svgRule
+      .oneOf('inline')
+      .resourceQuery(/inline/)
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .end()
+      .end()
+      .oneOf('external')
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]'
+      })
   }
 }
